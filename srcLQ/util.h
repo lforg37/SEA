9848af6c39d566c7c1be1b0be65a9a7eabd61a01
Luc Forget <luc.forget@insa-lyon.fr>
2015-11-30 09:54:25 +0100

    Virtual memory

--- srcLQ/util.h
+++
@@ -1,9 +0,0 @@
-#ifndef UTIL_H
-#define UTIL_H
-
-#define PANIC() do { kernel_panic(__FILE__,__LINE__) ; } while(0)
-#define ASSERT(exp) do { if(!(exp)) PANIC(); } while(0)
-
-void kernel_panic(char* string, int number);
-
-#endif
