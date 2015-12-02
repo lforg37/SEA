4527953443140bc70cf87c5c52bd61b66c894f64
Luc Forget <luc.forget@insa-lyon.fr>
2015-12-02 08:58:22 +0100

    vmem.c init termineeé

--- src/vmem.c
+++ src/vmem.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <stdbool.h>
 #include "vmem.h"
 #include "kheap.h"
 
@@ -10,11 +12,12 @@ static bool is_init_addr(uint32_t addr)
 
 uint32_t init_kern_translation_table(void)
 {
-	uint8_t* base_table = kAlloc_aligned(FIRST_LVL_TT_SIZE, 12);	
-	size_t kheap_page_count = __kernel_heap_end__ / PAGE_SIZE;
-	
+	uint8_t* base_table = kAlloc_aligned(FIRST_LVL_TT_SIZE, FIRST_LVL_BITSHIFT);
 	uint32_t device_flags = 0x437; 
 	uint32_t normal_flags = 0x44E;
+	uint32_t coarse_page_flag = 1;
+	
+	size_t kheap_page_count = __kernel_heap_end__ / PAGE_SIZE;
 
 	if(__kernel_heap_end__ % PAGE_SIZE > 0)
 		kheap_page_count++;
@@ -31,11 +34,13 @@ uint32_t init_kern_translation_table(void)
 	uint32_t* second_lvl_tt;
 	uint32_t virtual_addr = 0;
 	uint32_t* second_lvl_tt_entry;
+	uint32_t* first_lvl_tt = (uint32_t*) base_table;
 
 	/* Initialisation for kernel heap */
 	for (i = 0 ; i < kheap_first_lvl_tt_count; i++) {
-		second_lvl_tt = (uint32_t*) kAlloc_aligned(SECON_LVL_TT_SIZE, 8);	
+		second_lvl_tt = (uint32_t*) kAlloc_aligned(SECON_LVL_TT_SIZE, SECON_LVL_BITSHIFT);	
 		second_lvl_tt_entry = second_lvl_tt;
+		*first_lvl_tt++ = ((uint32_t)second_lvl_tt & 0xFFFFFC00) + coarse_page_flag; 
 
 		for (j = 0; j < SECON_LVL_TT_COUN ; j++) {
 			if (is_init_addr(virtual_addr)) {
@@ -47,9 +52,32 @@ uint32_t init_kern_translation_table(void)
 			virtual_addr += PAGE_SIZE;
 		}
 	}
-	for( ; virtual_addr < 0x20ffffff ; virtual_addr += SECON_LVL_TT_COUN * PAGE_SIZE )
-	{
-		
+
+	for ( ; virtual_addr < 0x20000000 ; virtual_addr += SECON_LVL_TT_COUN * PAGE_SIZE ) {
+		*first_lvl_tt++ = 0;
 	}
+	
+	for ( ; virtual_addr <= FIRST_LVL_START(0x20FFFFFF) ; ) {
+		second_lvl_tt = (uint32_t*) kAlloc_aligned(SECON_LVL_TT_SIZE, SECON_LVL_BITSHIFT);	
+		second_lvl_tt_entry = second_lvl_tt;
+		*first_lvl_tt++ = ((uint32_t)second_lvl_tt & 0xFFFFFC00) + coarse_page_flag; 
+
+		for (j = 0; j < SECON_LVL_TT_COUN ; j++) {
+			if (is_init_addr(virtual_addr)) {
+				*second_lvl_tt_entry = (virtual_addr & 0xFFFFF000)+ device_flags;	
+			} else {
+				*second_lvl_tt_entry = 0;	
+			}
+			second_lvl_tt_entry++;
+			virtual_addr += PAGE_SIZE;
+		}
+	}
+	
+	for ( ; virtual_addr <= FIRST_LVL_START(0xFFFFFFFF) ; 
+			virtual_addr += SECON_LVL_TT_COUN * PAGE_SIZE ) {
+		*first_lvl_tt++ = 0;
+	}
+
+	return 0;
 }
 
--- src/vmem.h
+++ src/vmem.h
@@ -4,9 +4,13 @@
 /* Taille d'une page en nombre d'adresse */
 #define PAGE_SIZE 4096 
 #define SECON_LVL_TT_COUN 256
+#define SECON_LVL_BITSHIFT 12
 #define SECON_LVL_TT_SIZE 1024
 #define FIRST_LVL_TT_COUN 4096
+#define FIRST_LVL_BITSHIFT 20
 #define FIRST_LVL_TT_SIZE 16384
+#define FIRST_LVL_START(ADDR) (ADDR & (0xFFFFFFFF - (( 1 << FIRST_LVL_BITSHIFT) -1)))
+#define SECON_LVL_START(ADDR) (ADDR & (0xFFFFFFFF - (( 1 << SECON_LVL_BITSHIFT) -1)))
 
 #include <stdint.h>
 
