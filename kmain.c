805fa97f7985a819e4be14da2acf9f28f74fa5ea
Rémy de Leydet <remy.deleydet@gmail.com>
2015-12-09 11:10:04 +0100

    Ajout du driver écran

--- kmain.c
+++ kmain.c
@@ -1,50 +1,36 @@
 #include "util.h"
 #include "syscall.h"
 #include "sched.h"
+#include "fb.h"
 
 void user_process_1()
 {
-	int v1 = 5;
 	while(1)
 	{
-		v1++;
+		
 	}
 }
 
-void user_process_2()
-{
-	int v2 = -12;
-	while(1)
-	{
-		v2 -= 2;
-		sys_wait(30);
-	}
-}
-
-void user_process_3() 
-{
-	int v3 = 0;
-	while(1)
-	{
-		v3 += 5;
-	}
-}
 
 void kmain( void )
 {
 	sched_init(PRIORITY);
+	FramebufferInitialize();
 	
-	create_process((func_t *)&user_process_1, 3);
- 	create_process((func_t *)&user_process_2, 3);
+	//create_process((func_t *)&user_process_1, 3);
 	
 	timer_init();
     ENABLE_IRQ();
 
 	__asm("cps 0x10"); // switch CPU to USER mode
 	
+	draw(0, 0, 0);
+	drawString("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est\n laborum.", 0, 30, 255, 0, 0);
+		
+	
 	// **********************************************************************
 	while(1)
 	{
-		sys_yield();
+		
 	}
 }
